961afdd8617517e000fb740fef659b08c2e9c7c9
Alex Peters <92709569+petersalex27@users.noreply.github.com>
2023-10-10 19:08:39 -0500

    application expr unit tests

--- parser/application_test.go
+++ parser/application_test.go
@@ -1,2 +1,62 @@
 package parser
 
+import (
+	"testing"
+
+	"github.com/petersalex27/yew-packages/expr"
+	"github.com/petersalex27/yew-packages/parser"
+	"github.com/petersalex27/yew-packages/parser/ast"
+	"github.com/petersalex27/yew-packages/source"
+	itoken "github.com/petersalex27/yew-packages/token"
+	"github.com/petersalex27/yew-packages/util/testutil"
+	"yew.lang/main/errors"
+	"yew.lang/main/token"
+)
+
+func TestApplication(t *testing.T) {
+	table := parser.
+		ForTypesThrough(_last_type_).
+		UseReductions().
+		Finally(parser.RuleSet(application__expr_expr_r,))
+
+	litToken := makeToken_test(token.IntValue, "1", 1, 1)
+	idToken := makeIdToken_test("a", 1, 1)
+
+	intExpr := ExpressionNode{Const(litToken)}
+	idExpr := ExpressionNode{Const(idToken)}
+
+	tests := []struct {
+		nodes  []ast.Ast
+		src    source.StaticSource
+		expect ast.AstRoot
+	}{
+		{
+			[]ast.Ast{idExpr, intExpr,},
+			parser.MakeSource("test/parser/application", "a 1"),
+			ast.AstRoot{
+				SomeExpression{Application, expr.Apply[token.Token](idExpr.Expression, intExpr.Expression)},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		p := parser.NewParser().
+			LA(1).
+			UsingReductionTable(table).Load(
+			[]itoken.Token{},
+			test.src,
+			nil, nil,
+		).InitialStackPush(test.nodes...)
+
+		actual := p.Parse()
+		if p.HasErrors() {
+			es := p.GetErrors()
+			errors.PrintErrors(es...)
+			t.Fatal(testutil.TestFail2("errors", nil, es, i))
+		}
+
+		if !actual.Equals(test.expect) {
+			t.Fatal(testutil.TestFail(test.expect, actual, i))
+		}
+	}
+}
